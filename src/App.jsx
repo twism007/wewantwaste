732ba61cbfd43907f37d8f2b4c193160a78630fe
twism007 <neagoeiuli@gmail.com>
2025-05-17 22:03:11 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     <>
       <div className="min-h-screen w-screen bg-[#121212] text-white">
         <TopBar activeStep="skip" />
+        <h1 className='text-center text-3xl font-semibold pt-4'>Choose Your Skip Size</h1>
+        <h3 className='text-center text-gray-400'>Select the skip size that best suits your needs</h3>
         <Product/>
       </div>
     </>
